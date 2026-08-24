5b17fc1769a64700ef1cedebd9725ad1f0572984
viarishi <viarishi@gmail.com>
2026-08-25 01:58:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='roxvia-ev-auth-gated-v1';
-const SHELL=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  const u=new URL(e.request.url); if(u.origin!==self.location.origin)return;
-  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))));
-});
